cc611be7652a2c9979fd00d90be99bf61b4ba311
shiroyasha68 <wira.yudha68@gmail.com>
2021-05-17 10:42:59 +0800

    first commit

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+let mawar = document.querySelector('.text-mawar');
+let anggrek = document.querySelector('.text-anggrek');
+let marigold = document.querySelector('.text-marigold');
+let melati = document.querySelector('.text-melati');
+let lavender = document.querySelector('.text-lavender');
+
+let textMawar =
+  '<p>mawar merah melambangkan cinta, kasih sayang yang dalam, dan kerinduan pada seseorang. Ketika seseorang memberikan mawar merah maka itu adalah cara untuk mengatakan bahan dirimu mencintai orang tersebut atau menyampaikan emosimu. </p>';
+let textAnggrek = '<p>anggrek menunjukkan bahwa proses keindahan bukanlah sesuatu yang mudah, tetapi membutuhkan perjuangan dan kesabaran dan bahwa dibutuhkan perjuangan dan kesabaran besar dalam hidup untuk menjadi sukses.</p>';
+
+let textMarigold = '<p>Bunga marigold adalah lambang dari kelahiran si bulan Oktber yang memiliki makna keindahan, kehangatan dari matahari, kreatifitas, kesuksesan, kebahagiaan dan kekayaan.</p>';
+
+let textMelati =
+  '<p>Bunga melati ini diambil dari sebuah kata bahasa Persia, yasmin yang mempunyai arti yaitu hadiah dari Tuhan. Secara filosofis, bunga melati menggambarkan kesucian serta ketulusan dan juga keanggunan dalam kesederhanaan.</p>';
+let textLavender = '<p>Lavender punya arti kesetiaan. Ia menjujung tinggi rasa percaya tanpa sedikitpun ingin berkhianat. Kalau ingin membuktikan kesetiaan, coba berikan bunga lavender sebagai lambang rasa setiamu padanya.</p>';
+
+mawar.innerHTML = textMawar;
+anggrek.innerHTML = textAnggrek;
+marigold.innerHTML = textMarigold;
+melati.innerHTML = textMelati;
+lavender.innerHTML = textLavender;
